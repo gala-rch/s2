32b5f51ab0880148a354260ccb67e99e01596fa6
gala-rch <gala.rachedi@gmail.com>
2025-04-01 11:10:52 +0200

    chang

--- td3/src/exo1.cpp
+++ td3/src/exo1.cpp
@@ -6,11 +6,8 @@ bool is_sorted(std::vector<int> const& vec) { return std::is_sorted(vec.begin(),
 void bubble_sort(std::vector<int> & vec) {
     size_t n = vec.size();
     
-    // Pour chaque itération, le plus grand élément "remonte" à la fin du tableau
     for (size_t i = 0; i < n - 1; i++) {
-        // Derniers i éléments sont déjà triés, donc on parcourt jusqu'à n-i-1
         for (size_t j = 0; j < n - i - 1; j++) {
-            // Si l'élément courant est plus grand que le suivant, on les échange
             if (vec[j] > vec[j + 1]) {
                 std::swap(vec[j], vec[j + 1]);
             }
--- td3/src/exo2.cpp
+++ td3/src/exo2.cpp
@@ -3,16 +3,13 @@
 bool is_sorted(std::vector<int> const& vec) { return std::is_sorted(vec.begin(), vec.end()); }
 
 void merge_sort_merge(std::vector<float> & vec, size_t const left, size_t const middle, size_t const right) {
-    // Créer des copies temporaires des sous-tableaux
     std::vector<float> left_array(vec.begin() + left, vec.begin() + middle + 1);
     std::vector<float> right_array(vec.begin() + middle + 1, vec.begin() + right + 1);
     
-    // Indices pour parcourir les sous-tableaux temporaires
     size_t left_index = 0;
     size_t right_index = 0;
     size_t merge_index = left;
     
-    // Fusionner les deux sous-tableaux triés
     while (left_index < left_array.size() && right_index < right_array.size()) {
         if (left_array[left_index] <= right_array[right_index]) {
             vec[merge_index] = left_array[left_index];
@@ -24,14 +21,12 @@ void merge_sort_merge(std::vector<float> & vec, size_t const left, size_t const 
         merge_index++;
     }
     
-    // Copier les éléments restants du sous-tableau gauche
     while (left_index < left_array.size()) {
         vec[merge_index] = left_array[left_index];
         left_index++;
         merge_index++;
     }
     
-    // Copier les éléments restants du sous-tableau droit
     while (right_index < right_array.size()) {
         vec[merge_index] = right_array[right_index];
         right_index++;
@@ -40,19 +35,15 @@ void merge_sort_merge(std::vector<float> & vec, size_t const left, size_t const 
 }
 
 void merge_sort(std::vector<float> & vec, size_t const left, size_t const right) {
-    // Condition d'arrêt: tableau de taille 1 ou vide
     if (left >= right) {
         return;
     }
     
-    // Trouver le milieu du tableau
     size_t middle = left + (right - left) / 2;
     
-    // Trier récursivement les deux moitiés
     merge_sort(vec, left, middle);
     merge_sort(vec, middle + 1, right);
     
-    // Fusionner les deux moitiés triées
     merge_sort_merge(vec, left, middle, right);
 }
 
